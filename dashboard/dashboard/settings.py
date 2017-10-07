3d009bb5b96f821b6a7f9f96580e534677dd296b
Andrea Gilardoni <andrea.gilardoni96@gmail.com>
2017-10-07 20:10:34 +0200

    Configured docker-compose to run with mongodb and gunicorn

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -80,6 +80,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#    'default' : {
+#        'ENGINE': 'django_mongodb_engine',
+#         #'NAME': 'my_database',
+#         #'HOST': 'mongodb',
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
